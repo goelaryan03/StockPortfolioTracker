72a12e1154190d05be9c3a9b6113486ffe06bb9f
goelaryan03 <aryangoel57128@gmail.com>
2024-12-19 15:52:47 +0530

    Starting my project

---
+++ src/main/java/com/assignment/CapX/CapXApplication.java
@@ -0,0 +1,13 @@
+package com.assignment.CapX;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CapXApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CapXApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/assignment/CapX/CapXApplicationTests.java
@@ -0,0 +1,13 @@
+package com.assignment.CapX;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CapXApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
